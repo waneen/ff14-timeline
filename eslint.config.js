d280e12026d5a6dff7dd00b3073e418f6a9b1d5a
waneen <0629yamataka@gmail.com>
2024-10-15 14:27:46 +0000

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+/** @type {import('eslint/lib/shared/types').ConfigData} */
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:vitest/recommended",
+    "prettier",
+    "plugin:storybook/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "import","prettier"],
+  settings: {
+    react: { version: "detect" },
+  },
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    // "vitest/consistent-test-it": ["error", { fn: "test" }],
+    "import/order": [
+      "warn",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          ["parent", "sibling"],
+          "object",
+          "type",
+          "index",
+        ],
+        "newlines-between": "always",
+        pathGroupsExcludedImportTypes: ["builtin"],
+        alphabetize: { order: "asc", caseInsensitive: true },
+        pathGroups: [
+          {
+            pattern: "react",
+            group: "external",
+            position: "before",
+          },
+        ],
+      },
+    ],
+  },
+};
